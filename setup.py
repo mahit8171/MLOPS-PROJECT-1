be7c17646448b4b5c80c4f0969deab43dc49c333
mahit8171 <111651529+mahit8171@users.noreply.github.com>
2025-12-22 16:28:43 +0530

    structure complete of this project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="MOHIT KUMAR",
+    author_email="mohitkumar989419@gmail.com",
+    packages=find_packages()
+)
